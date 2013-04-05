eef53c360a2bd65d40a03b17acf4ecc6030c504a
Hans-Peter Schadler <hps@abyle.org>
2013-04-05 17:31:33 +0200

    Added percolation find algorithm

--- findcluster.cpp
+++ findcluster.cpp
@@ -30,7 +30,8 @@ vector<int> isinsector;  // isinsector[is] stores the sector of the lattice poin
 vector<int> clustersector; // clustersector[c] stores the sector of the cluster c
 vector<int> isincluster;  // isincluster[is] stores the cluster of the lattice point is
 vector<vector<int> > clustermembers; // clustermembers[c][i] stores the members/lattice points of the cluster c (0 < i < N_c)
-vector<int> numberofcmembers; // numberofcmembers[c] stores the number of members of the cluster c
+
+vector<int> percolatingclusters;
 
 void fillneib();
 int init(int &argc, char *argv[]);
@@ -40,13 +41,18 @@ void fillsectors(double fraction);
 void findclusters();
 void checkclusters();
 
+void findpercolatingcluster();
+
 int latmap(int i1, int i2, int i3);
 
+double fraction = 0.9;
+
 int main(int argc, char *argv[]){
-	double fraction;
+
 	// Handle command line information and initialize arrays and other stuff...
 	if(init(argc, argv) != 0){
 		cout << "Error in init()!" << endl;
+		return 1;
 	}
 
 	cout << "Reading 3d lattice with Polyakov loop ev's... " << flush;
@@ -55,8 +61,6 @@ int main(int argc, char *argv[]){
 	}
 	cout << "done!" << endl;
 	checkPollEv(leng1, leng2, leng3, leng4, matrixdim, pollev);
-	
-	fraction = 0.9;
 	
 	// Main part
 	// Categorize lattice points by sectors
@@ -68,19 +72,30 @@ int main(int argc, char *argv[]){
 	cout << "Checking clusters... " << flush;
 	checkclusters();
 	cout << "done!" << endl;
-
+	
+	cout << "Searching for percolating clusters... " << flush;
+	findpercolatingcluster();
+	cout << endl;
+	
+	cout << endl << percolatingclusters.size() << " of " << clustermembers.size() << " clusters are percolating!" << endl;
+	for(unsigned int c=0;c<percolatingclusters.size();c++){
+		cout << "Cluster " << percolatingclusters[c] << " is percolating!" << endl;
+		cout << "The cluster has " << clustermembers[c].size() << " members and is in sector " << clustersector[c] << " !" << endl;
+	}
+	
 	return 0;
 }
 
 int init(int &argc, char *argv[]){
-	if(argc<4){
-		cout << "./findcluster.x Ns Nt binevconfin" << endl;
+	if(argc<5){
+		cout << "./findcluster.x Ns Nt binevconfin fraction" << endl;
 		return 1;
 	}
 
 	Ns=atoi(argv[1]);
 	Nt=atoi(argv[2]);
 	fevname = argv[3];
+	fraction = atof(argv[4]);
 	
 	leng1=Ns; leng2=Ns; leng3=Ns; leng4=Nt;
 	
@@ -148,6 +163,56 @@ void fillsectors(double fraction){
 	#endif
 }
 
+void findpercolatingcluster(){
+	// Idea from Fortran code
+	int is=0;
+	
+	int cluster=0;
+	
+	vector<vector<vector<int> > > members;
+	members.resize(clustermembers.size());
+	for(unsigned int i=0;i<members.size();i++){
+		members[i].resize(3);
+		members[i][0].resize(leng1);
+		for(int j=0;j<leng1;j++)
+			members[i][0][j]=0;
+		members[i][1].resize(leng2);
+		for(int j=0;j<leng2;j++)
+			members[i][1][j]=0;
+		members[i][2].resize(leng3);
+		for(int j=0;j<leng3;j++)
+			members[i][2][j]=0;
+	}
+	
+	for(int i1=0;i1<leng1;i1++)
+	for(int i2=0;i2<leng2;i2++)
+	for(int i3=0;i3<leng3;i3++){
+		is = latmap(i1, i2, i3);
+		cluster = isincluster[is];
+		if(clustersector[cluster] < 2){
+			members[cluster][0][i1] = 1;
+			members[cluster][1][i2] = 1;
+			members[cluster][2][i3] = 1;
+		}
+	}
+	
+	int sum1, sum2, sum3;
+	
+	for(unsigned int c=0;c<clustermembers.size();c++){
+		sum1=0; sum2=0; sum3=0;
+		for(int i1=0;i1<leng1;i1++)
+			sum1 += members[c][0][i1];
+		for(int i2=0;i2<leng2;i2++)
+			sum2 += members[c][1][i2];
+		for(int i3=0;i3<leng3;i3++)
+			sum3 += members[c][2][i3];
+			
+		if(sum1 == leng1 || sum2 == leng2 || sum3 == leng3){
+			percolatingclusters.push_back(c);
+		}
+	}
+}
+
 void findclusters(){
 	int sector = 0, cluster = 0, memberis = 0, isneib = 0;
 
@@ -160,34 +225,34 @@ void findclusters(){
 	
 	int totalmembers=0;
 	
-	cluster = 0;
+	cluster = -1; // Note: We begin counting with ID 0!
 	for(int is=0;is<Nspace;is++){
 		if(isfiled[is] == false){
 			sector = isinsector[is];
-			cluster++; // current cluster
+			cluster++; // current cluster id (Note: We begin counting with ID 0!)
 			clustersector.push_back(sector); // Current cluster is in sector sector
 			clustermembers.push_back(vector<int>()); // Add a new cluster
 			
-			clustermembers[cluster-1].push_back(is); // Add is to this new cluster
+			clustermembers[cluster].push_back(is); // Add is to this new cluster
 			isincluster[is] = cluster; // Set the cluster id for the point  is
 			isfiled[is] = true; // Lattice point is now filed
 			
 			// Iterate over all found points in the actual cluster (loop does not have fixed length)
 			int member = 0;
-			while((unsigned int)member < clustermembers[cluster-1].size()){
-				memberis = clustermembers[cluster-1][member];
+			while((unsigned int)member < clustermembers[cluster].size()){
+				memberis = clustermembers[cluster][member];
 			
 				for(int mu=0;mu<6;mu++){
 					isneib = neib[memberis][mu];
 					if(isfiled[isneib] == false && isinsector[isneib] == sector){
-						clustermembers[cluster-1].push_back(isneib);
+						clustermembers[cluster].push_back(isneib);
 						isincluster[isneib] = cluster;
 						isfiled[isneib] = true;
 					}
 				}
 				
 				#ifdef DEBUG
-				cout << "Cluster " << cluster << " member " << member + 1 << " of " << clustermembers[cluster-1].size() << " members." << endl;
+				cout << endl <<  "Cluster " << cluster << " member " << member + 1 << " of " << clustermembers[cluster].size() << " members." << endl;
 				#endif
 				
 				member++;
@@ -209,7 +274,7 @@ void findclusters(){
 void checkclusters(){
 	for(int is=0;is<Nspace;is++){
 		if(isincluster[is] < 0)
-			cout << "WARNING: Some points do not have a cluster ID!" << endl;
+			cout << "WARNING: Some points do not have a valid cluster ID!" << endl;
 	}
 	
 	int sum=0;
