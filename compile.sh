ee5dd827ca98f45ff14a5234c76f272a9deda81f
Hans-Peter Schadler <hps@abyle.org>
2013-04-08 16:44:21 +0200

    Crosscheck with Gergo ok

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-g++ -O3 -o bin/genpollhisto.x genpollhisto.cpp -lgsl -lblas
-g++ -O3 -o bin/genpollev_from_config.x genpollev_from_config.cpp -lgsl -lblas -llapack
-g++ -O3 -o bin/genpollev_from_lpoll.x genpollev_from_lpoll.cpp -lgsl -lblas -llapack
+g++ -O3 -o bin/genpollhisto.x genpollhisto.cpp -lgsl -lgslcblas
+g++ -O3 -o bin/genpollev_from_config.x genpollev_from_config.cpp -lgsl -lgslcblas -llapack
+g++ -O3 -o bin/genpollev_from_lpoll.x genpollev_from_lpoll.cpp -lgsl -lgslcblas -llapack
 g++ -O3 -Wall -o bin/findcluster_debug.x findcluster.cpp -DDEBUG
 g++ -O3 -Wall -o bin/findcluster.x findcluster.cpp
