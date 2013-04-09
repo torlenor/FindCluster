35c3a37f79b6602ee8c23831000b6f5e39b5ab4d
Hans-Peter Schadler <hps@abyle.org>
2013-04-09 17:40:46 +0200

    Linked libGL explizitly

--- compile_3dclusters.sh
+++ compile_3dclusters.sh
@@ -3,5 +3,5 @@ if [ "Darwin" = `uname` ]
 then
 	g++ 3dclusters.cpp -O3 -framework OpenGL -framework glut -o ./bin/3dclusters.x
 else
-	g++ 3dclusters.cpp -O3 -lglut -lGLU -o ./bin/3dclusters.x
+	g++ 3dclusters.cpp -O3 -lglut -lGLU -o ./bin/3dclusters.x -lGL
 fi
