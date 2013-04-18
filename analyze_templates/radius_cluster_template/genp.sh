fb73b89f06d9df6a1ceb0f517862c4ec8a2667ca
Hans-Peter Schadler <hps@abyle.org>
2013-04-18 11:23:40 +0200

    Added analyze templates

---
+++ analyze_templates/radius_cluster_template/genp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+meas=400
+databasedir="../../b6.2"
+radius=0.85
+for t in 2 3 4 5 6 7 8 9 10 11 12 14 16 18 20 ; do 
+	echo "#\!/bin/bash" > c40x${t}.sh
+        echo "#" >> c40x${t}.sh
+        echo "#$ -cwd" >> c40x${t}.sh
+        echo "#$ -S /bin/csh" >> c40x${t}.sh
+        echo "#$ -V" >> c40x${t}.sh
+        echo "#$ -q all.q" >> c40x${t}.sh
+        echo "#$ -N analyze${t}" >> c40x${t}.sh
+	echo "cd ${PWD}/40x${t}" >> c40x${t}.sh
+	echo "/usr/bin/time -o ../40x${t}.log --append ../findcluster.x -s 40 -t $t -n $meas -r $radius pollev_40x${t}_b6.20.list -b > ../40x${t}.log" >> c40x${t}.sh
+	echo "exit 0" >> c40x${t}.sh
+done
+
+for t in 2 3 4 5 6 7 8 9 10 11 12 14 16 18 20 ; do
+	if [ ! -d 40x${t} ]
+		then
+	        mkdir 40x${t}
+	fi
+	cd 40x${t}
+	pwd
+	rm -f pollev_40x${t}_b6.20.list
+	ls ${databasedir}/40x${t}/pollev_*m* > pollev_40x${t}_b6.20.list
+	cd ..
+done
