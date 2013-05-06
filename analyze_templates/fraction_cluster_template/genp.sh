b79de815339de8d65ddc3e6fb2410f2f93bea70f
Hans-Peter Schadler <hps@abyle.org>
2013-05-06 10:55:56 +0200

    Major update to genp.sh clrun generation script

--- analyze_templates/fraction_cluster_template/genp.sh
+++ analyze_templates/fraction_cluster_template/genp.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
-meas=400
+Ns=40
+Nt="2 3 4 5 6 7 8 9 10 11 12 14 16 18 20"
+meas=200
+
+box=0 # 0/1 to disable/enable box counting
+
 databasedir="/cl_tmp/07schadh/su3runs/su3pp/bin"
-fraction=0.85
-for t in 2 3 4 5 6 7 8 9 10 11 12 14 16 18 20 ; do 
-	echo "#\!/bin/bash" > c40x${t}.sh
-        echo "#" >> c40x${t}.sh
-        echo "#$ -cwd" >> c40x${t}.sh
-        echo "#$ -S /bin/csh" >> c40x${t}.sh
-        echo "#$ -V" >> c40x${t}.sh
-        echo "#$ -q all.q" >> c40x${t}.sh
-        echo "#$ -N analyze${t}" >> c40x${t}.sh
-	echo "setenv MV2_ENABLE_AFFINITY 0"  >> c40x${t}.sh
-	echo "cd ${PWD}/40x${t}" >> c40x${t}.sh
-	echo "/usr/bin/time -o ../40x${t}.log --append ../findcluster.x -s 40 -t $t -n $meas -f $fraction pollev_40x${t}_b6.20.list -b > ../40x${t}.log" >> c40x${t}.sh
-	echo "exit 0" >> c40x${t}.sh
+fraction=0.00
+for t in $Nt ; do 
+	echo "#\!/bin/bash" > c${Ns}x${t}.sh
+        echo "#" >> c${Ns}x${t}.sh
+        echo "#$ -cwd" >> c${Ns}x${t}.sh
+        echo "#$ -S /bin/csh" >> c${Ns}x${t}.sh
+        echo "#$ -V" >> c${Ns}x${t}.sh
+        echo "#$ -q all.q" >> c${Ns}x${t}.sh
+        echo "#$ -N analyze${t}" >> c${Ns}x${t}.sh
+	echo "setenv MV2_ENABLE_AFFINITY 0"  >> c${Ns}x${t}.sh
+	echo "cd ${PWD}/${Ns}x${t}" >> c${Ns}x${t}.sh
+	if [ $box -eq 1 ] ; then
+		echo "/usr/bin/time -o ../${Ns}x${t}.log --append ../findcluster.x -s $Ns -t $t -n $meas -f $fraction pollev_${Ns}x${t}.list -b > ../${Ns}x${t}.log" >> c${Ns}x${t}.sh
+	else
+		echo "/usr/bin/time -o ../${Ns}x${t}.log --append ../findcluster.x -s $Ns -t $t -n $meas -f $fraction pollev_${Ns}x${t}.list > ../${Ns}x${t}.log" >> c${Ns}x${t}.sh
+	fi
+	echo "exit 0" >> c${Ns}x${t}.sh
 done
 
-for t in 2 3 4 5 6 7 8 9 10 11 12 14 16 18 20 ; do
-	if [ ! -d 40x${t} ]
+for t in $Nt ; do
+	if [ ! -d ${Ns}x${t} ]
 		then
-	        mkdir 40x${t}
+	        mkdir ${Ns}x${t}
 	fi
-	cd 40x${t}
+	cd ${Ns}x${t}
 	pwd
-	rm -f pollev_40x${t}_b6.20.list
-	ls ${databasedir}/40x${t}/pollev_*m* > pollev_40x${t}_b6.20.list
+	rm -f pollev_${Ns}x${t}.list
+	ls ${databasedir}/${Ns}x${t}/pollev_*m* > pollev_${Ns}x${t}.list
 	cd ..
 done
