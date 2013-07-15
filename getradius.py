59c93d7e8a3a81ca8d5cc17b9db84789e3e45935
Hans-Peter Schadler <hps@abyle.org>
2013-07-15 15:28:32 +0200

    Increased output accuracy of getradius.py

--- getradius.py
+++ getradius.py
@@ -37,29 +37,13 @@ if radius2>givenradius:
 
 cnt=0
 
-F1needscalc=False
-F2needscalc=False
-
 bestdelta=float('inf')
 bestradius=0
 bestF=0
 
-
 while (delta > deltagoal and cnt < maxcnt) :
 	Fnew = 0.5*(F1+F2)
 
-	if F1needscalc:
-		runcmdstr = str.replace(cmdstr, "FRACTION", repr(F1));
-		cmd = subprocess.Popen(runcmdstr, shell=True, stdout=subprocess.PIPE)
-		first_line = cmd.stdout.readline()
-		radius1=float(str.strip(first_line));
-	
-	if F2needscalc:
-		runcmdstr = str.replace(cmdstr, "FRACTION", repr(F2));
-		cmd = subprocess.Popen(runcmdstr, shell=True, stdout=subprocess.PIPE)
-		first_line = cmd.stdout.readline()
-		radius2=float(str.strip(first_line));
-	
 	runcmdstr = str.replace(cmdstr, "FRACTION", repr(Fnew));
 	cmd = subprocess.Popen(runcmdstr, shell=True, stdout=subprocess.PIPE)
 	first_line = cmd.stdout.readline()
@@ -67,10 +51,10 @@ while (delta > deltagoal and cnt < maxcnt) :
 
 	if radiusnew<givenradius:
 		F2=Fnew
-		F2needscalc=True
+		radius2=radiusnew
 	else:
 		F1=Fnew
-		F1needscalc=True
+		radius1=radiusnew
 
 	delta = abs(radiusnew - givenradius)
 	if delta<bestdelta:
@@ -78,42 +62,23 @@ while (delta > deltagoal and cnt < maxcnt) :
 		bestradius=radiusnew
 		bestF=Fnew
 
-	print("Radius1 = {0:.5f}".format(radius1))
-	print("Radius2 = {0:.5f}".format(radius2))
-	print("Radiusnew = {0:.5f}".format(radiusnew))
-	print("F1 = {0:.5f}".format(F1))
-	print("F2 = {0:.5f}".format(F2))
-	print("Fnew = {0:.5f}".format(Fnew))
-	print("Delta = {0:.5f}".format(delta))
+	print("Radius1 = {0:.8f}".format(radius1))
+	print("Radius2 = {0:.8f}".format(radius2))
+	print("Radiusnew = {0:.8f}".format(radiusnew))
+	print("F1 = {0:.8f}".format(F1))
+	print("F2 = {0:.8f}".format(F2))
+	print("Fnew = {0:.8f}".format(Fnew))
+	print("Delta = {0:.8f}".format(delta))
 	print("")
 
 	cnt = cnt+1
 
-print("")
-print("Starting second phase...")
-cnt = 1
-F1=Fnew-0.0005
-F2=Fnew+0.0005
-
-runcmdstr = str.replace(cmdstr, "FRACTION", repr(F1));
-cmd = subprocess.Popen(runcmdstr, shell=True, stdout=subprocess.PIPE)
-first_line = cmd.stdout.readline()
-radius1=float(str.strip(first_line));
-
-runcmdstr = str.replace(cmdstr, "FRACTION", repr(F2));
-cmd = subprocess.Popen(runcmdstr, shell=True, stdout=subprocess.PIPE)
-first_line = cmd.stdout.readline()
-radius2=float(str.strip(first_line));
-
-delta1 = abs(radius1 - givenradius)
-delta2 = abs(radius2 - givenradius)
-
 print("------------------------------------------------------------------------")
 print("Final results after {0:.0f} iterations:".format(cnt))
 print("")
-print("Radiusnew = {0:.5f}".format(bestradius))
-print("Fnew = {0:.5f}".format(bestF))
-print("Delta = {0:.5f}".format(bestdelta))
+print("Radiusnew = {0:.8f}".format(bestradius))
+print("Fnew = {0:.8f}".format(bestF))
+print("Delta = {0:.8f}".format(bestdelta))
 print("")
 print("Have a nice day!")
 
