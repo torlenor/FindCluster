4e9ad2bc5ccbbb381471e1e41ed0f7cf2a4e9d3a
Hans-Peter Schadler <hps@abyle.org>
2013-07-22 10:26:35 +0200

    Updated compile scripts

--- compile.sh
+++ compile.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-g++ -O3 -o bin/genpollhisto.x genpollhisto.cpp -lgsl -lgslcblas
-#g++ -O3 -o bin/genpollhisto1d.x genpollhisto1d.cpp -lgsl -lgslcblas
-g++ -O3 -o bin/genpollhisto1d_jack.x genpollhisto1d_jack.cpp -lgsl -lgslcblas
-
 # g++ -O3 -o bin/genpollev_from_config.x genpollev_from_config.cpp -lgsl -lgslcblas -llapack
 # g++ -O3 -o bin/genpollev_from_lpoll.x genpollev_from_lpoll.cpp -lgsl -lgslcblas -llapack
 # g++ -O3 -Wall -o bin/findcluster_debug.x findcluster.cpp -DDEBUG
